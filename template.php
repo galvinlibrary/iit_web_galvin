4f83d3c56be610f0316f1b3eab0b4444866865a9
Christine McClure <camcclure@gmail.com>
2015-04-02 12:16:06 -0500

    removed extraneious count check

--- template.php
+++ template.php
@@ -37,11 +37,9 @@ function iit_web_galvin_breadcrumb($variables) {
   
   $tmp = array_pop($breadcrumb); // remove the last element of the array, which is the static page title
   array_push($breadcrumb, '<span class="active">' . drupal_get_title() . '</span>'); // add current page title (handles context. filter from view)
-  if (count($breadcrumb) > 0) {
-    $crumbs= implode($sep, $breadcrumb) . $sep;
-    $crumbs = preg_replace("/$sep$/", '', $crumbs);
-    return $crumbs;
-  }
+  $crumbs= implode($sep, $breadcrumb) . $sep;
+  $crumbs = preg_replace("/$sep$/", '', $crumbs);
+  return $crumbs;
 }
 
 function iit_web_galvin_preprocess_html(&$vars) {
